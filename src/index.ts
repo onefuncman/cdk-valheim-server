2a59eb1e03cede8e508f7e9c63bdf935c6fd3c77
onefuncman <45834012+onefuncman@users.noreply.github.com>
2022-11-27 02:41:31 +0000

    collapse to single file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import { PublicIPSupport, Route53DomainProps } from './public_ip_support';
 import { Names, RemovalPolicy, Tags } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as efs from 'aws-cdk-lib/aws-efs';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
+import * as path from 'path';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Duration } from 'aws-cdk-lib';
 
 
 const VALHEIM_PORT = 2456;
@@ -225,5 +231,79 @@ export class ValheimServer extends Construct {
         },
       ],
     });
+  }
+}
+
+
+export interface Route53DomainProps {
+  assumedRole?: string;
+  hostzedZone: string;
+  domainName: string;
+}
+
+export interface PublicIPSupportProps {
+  cluster: ecs.ICluster;
+  dnsConfig?: Route53DomainProps;
+}
+
+export class PublicIPSupport extends Construct {
+
+  constructor(scope: Construct, id: string, props: PublicIPSupportProps) {
+    super(scope, id);
+
+    const cluster = props.cluster;
+
+    const func = new lambda.Function(this, 'PublicIPManager', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'functions/public_ip_manager')),
+      timeout: Duration.seconds(10),
+      logRetention: RetentionDays.TWO_WEEKS,
+      initialPolicy: [
+        new PolicyStatement({
+          actions: ['ec2:DescribeNetworkInterfaces'],
+          resources: ['*']
+        }),
+      ]
+    });
+
+    if (props.dnsConfig) {
+      func.addEnvironment("DNS_HOSTED_ZONE", props.dnsConfig.hostzedZone)
+      func.addEnvironment("DNS_DOMAIN", props.dnsConfig.domainName)
+
+      /**
+       * If we are assume a role, we just need those permissions.
+       * If we are not assuming a role, we need permissions for route53
+       */
+      if (props.dnsConfig.assumedRole) {
+        func.addEnvironment("DNS_ASSUMED_ROLE", props.dnsConfig.assumedRole)
+        func.addToRolePolicy(
+          new PolicyStatement({
+            actions: ['sts:AssumeRole'],
+            resources: [props.dnsConfig.assumedRole]
+          }),
+        );
+      } else {
+        func.addToRolePolicy(
+          new PolicyStatement({
+            actions: ['route53:ChangeResourceRecordSets', 'route53:ListResourceRecordSets'],
+            resources: [props.dnsConfig?.hostzedZone]
+          })
+        )
+      }
+    }
+
+    new events.Rule(this, 'ServiceRule', {
+      eventPattern: {
+        source: ['aws.ecs'],
+        detailType: ['ECS Task State Change'],
+        detail: {
+          clusterArn: [cluster.clusterArn],
+          lastStatus: ['RUNNING'],
+        }
+      },
+      targets: [new targets.LambdaFunction(func)]
+    });
+
   }
 }
